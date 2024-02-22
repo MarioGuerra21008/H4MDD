cfc5f00b410b664e9c9ef0f6dad895a83b91587a
Javier Alvarado <77702824+XaviAlvarado18@users.noreply.github.com>
2024-02-22 17:11:40 -0600

    Data normalizada y grupo de clusteres

--- H3.R
+++ H3.R
@@ -1,16 +1,33 @@
+
 # Cargar datos desde un archivo CSV
 datos <- read.csv("train.csv", header = TRUE, encoding = "UTF-8")
 
+View(datos$MiscVal)
+
+Summary(datos)
+
 View(datos)
 
-summary(datos)
-
-# Preprocesamiento de datos para clustering y análisis de grupos.
-
-datos_para_clustering <- datos[, c("MSSubClass", "LotFrontage", "LotArea", "OverallQual", "OverallCond", "YearBuilt",
+datos_para_clustering <- datos[, c("LotFrontage", "LotArea", "OverallQual", "OverallCond", "YearBuilt",
                                    "YearRemodAdd", "TotalBsmtSF", "BsmtFullBath", "BsmtHalfBath", "FullBath", "HalfBath",
-                                   "KitchenQual", "TotRmsAbvGrd", "Fireplaces", "GarageYrBlt", "GarageCars", "GarageArea",
+                                    "TotRmsAbvGrd", "Fireplaces", "GarageYrBlt", "GarageCars", "GarageArea",
                                    "PoolArea", "MiscVal", "MoSold", "YrSold", "SalePrice")]
 
-datos_para_clustering <- na.omit(datos_para_clustering) # Eliminar filas con valores faltantes
+datos_para_clustering <- na.omit(datos_para_clustering) 
 
+normalized_data <- scale(datos_para_clustering)
+
+View(normalized_data)
+
+#Obtener cantidad de clusteres
+set.seed(123)
+k_values <- 1:10
+iner <- numeric(length(k_values))
+
+for (k in k_values) {
+  model <- kmeans(normalized_data, centers = k)
+  iner[k] <- model$tot.withinss
+}
+
+plot(k_values, iner, type = "b", main = "Método del Codo", xlab = "Número de Clústeres (k)", ylab = "Inercia")
+abline(v = which.min(diff(iner) > 10) + 1, col = "red", lty = 2)

a348a223380718e834c81c9e2088d7bd9a6a0553
Mario Guerra <maguerramorales@gmail.com>
2024-02-22 16:56:34 -0600

    Inicio de Clustering

--- H3.R
+++ H3.R
@@ -1,6 +1,16 @@
 # Cargar datos desde un archivo CSV
 datos <- read.csv("train.csv", header = TRUE, encoding = "UTF-8")
 
+View(datos)
+
 summary(datos)
 
+# Preprocesamiento de datos para clustering y análisis de grupos.
+
+datos_para_clustering <- datos[, c("MSSubClass", "LotFrontage", "LotArea", "OverallQual", "OverallCond", "YearBuilt",
+                                   "YearRemodAdd", "TotalBsmtSF", "BsmtFullBath", "BsmtHalfBath", "FullBath", "HalfBath",
+                                   "KitchenQual", "TotRmsAbvGrd", "Fireplaces", "GarageYrBlt", "GarageCars", "GarageArea",
+                                   "PoolArea", "MiscVal", "MoSold", "YrSold", "SalePrice")]
+
+datos_para_clustering <- na.omit(datos_para_clustering) # Eliminar filas con valores faltantes
 
